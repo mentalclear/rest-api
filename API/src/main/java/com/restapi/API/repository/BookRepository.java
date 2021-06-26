b4677aefd8aaa1acae2c4258625519bf040cdadc
Dmitrii Kilishek <dmitrii.kilishek@gmail.com>
2021-06-26 08:45:45 -0400

    Working Prototype

---
+++ API/src/main/java/com/restapi/API/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.restapi.API.repository;
+
+import com.restapi.API.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
